bcca7fb30762e70f8395030d56e611fdfa746de6
san7iix <xsantysxx@gmail.com>
2018-11-29 13:37:12 -0500

    Correción de un error en el registro

--- vista/adds/registro.php
+++ vista/adds/registro.php
@@ -6,7 +6,7 @@
   if(Usuariocontrolador::obtenerUsuario($_POST['txtCodigo'])) echo "El usuario ya existe";
   else {
     try {
-      UsuarioControlador::registrar($_POST['txtCodigo'],$_POST['txtNombre1'],$_POST['txtNombre2'],$_POST['txtApellido1'],$_POST['txtApellido2'],$_POST['txtContra']);
+      UsuarioControlador::registrar($_POST['txtCodigo'],$_POST['txtNombre1'],$_POST['txtNombre2'],$_POST['txtApellido1'],$_POST['txtApellido2'],$_POST['txtContra'],'1');
       echo true;
     } catch (\Exception $e) {
 
